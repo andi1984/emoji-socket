9c9a5a2177f4da27ca30ee12af5e8cae6f93acc1
Andreas Sander <mail@andi1984.de>
2021-01-30 23:09:43 +0100

    Setup express server infrastructure

--- client/ws-connect.ts
+++ client/ws-connect.ts
@@ -1,58 +1,56 @@
-// Create WebSocket connection.
-const socket = new WebSocket("ws://localhost:4000");
-
-const randomNumber = Math.random();
-
-// Connection opened
-socket.addEventListener("open", function (event) {
-  console.log("Connected to websocket");
-});
-
-// Overlay Data handler
-// Listen for messages from the websocket server
-const messagesListElement = document.querySelector('[data-js="messages"]');
-const canvas = <HTMLCanvasElement>document.getElementById("canvas");
-
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-const getRandomX = (): number => {
-  return Math.floor(Math.random() * Math.floor(canvas.width));
+const drawEmojiOnCanvas = (canvas: HTMLCanvasElement, emoji: string) => {
+  const ctx = canvas.getContext("2d");
+  if (!!ctx) {
+    ctx.font = `${getRandomFontSize()}px serif`;
+    ctx.fillText(
+      emoji,
+      getRandomMax(canvas.width),
+      getRandomMax(canvas.height)
+    );
+  }
 };
 
-const getRandomY = (): number => {
-  return Math.floor(Math.random() * Math.floor(canvas.height));
+const getRandomMax = (max: number): number => {
+  return Math.floor(Math.random() * Math.floor(max));
 };
 
 const getRandomFontSize = (): number => {
   return Math.max(Math.floor(Math.random() * Math.floor(100)), 40);
 };
 
-const drawEmojiOnCanvas = (emoji: string) => {
-  const ctx = canvas.getContext("2d");
-  if (!!ctx) {
-    ctx.font = `${getRandomFontSize()}px serif`;
-    ctx.fillText(emoji, getRandomX(), getRandomY());
-  }
-};
-socket.addEventListener("message", function (event) {
-  console.log("Message from server ", event.data);
-  if (!!messagesListElement) {
-    const emoji = event.data;
-    drawEmojiOnCanvas(emoji);
+fetch("./ws", { method: "POST" })
+  .then((response) => response.json())
+  .then(({ ws }) => {
+    // Create WebSocket connection.
+    const socket = new WebSocket(ws);
 
-    // const liElement = document.createElement("li");
-    // liElement.textContent = emoji;
+    // Connection opened
+    socket.addEventListener("open", function (event) {
+      console.log("Connected to websocket");
+    });
 
-    // messagesListElement.appendChild(liElement);
-  }
-});
+    // Overlay Data handler
+    // Listen for messages from the websocket server
+    const canvas = <HTMLCanvasElement>document.getElementById("canvas");
 
-// Emoji Button Click handler
-const buttonElements = document.querySelectorAll("button");
-buttonElements.forEach((buttonElement: HTMLButtonElement) => {
-  buttonElement.addEventListener("click", () => {
-    // Send the Emoji
-    socket.send(buttonElement.value);
-  });
-});
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    socket.addEventListener("message", function (event) {
+      console.log("Message from server ", event.data);
+      if (!!canvas) {
+        const emoji = event.data;
+        drawEmojiOnCanvas(canvas, emoji);
+      }
+    });
+
+    // Emoji Button Click handler
+    const buttonElements = document.querySelectorAll("button");
+    buttonElements.forEach((buttonElement: HTMLButtonElement) => {
+      buttonElement.addEventListener("click", () => {
+        // Send the Emoji
+        socket.send(buttonElement.value);
+      });
+    });
+  })
+  .catch(console.error);
--- server/app.ts
+++ server/app.ts
@@ -1,9 +1,12 @@
 import WebSocket from "./node_modules/@types/ws";
+import { Request, Response } from "./node_modules/@types/express";
 
 const ws = require("ws");
+const express = require("express");
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
 
-const port = 4000;
-const wss = new ws.Server({ port });
+const wss = new ws.Server({ port: process.env.WEBSOCKET_PORT });
 
 wss.on("connection", function connection(client: WebSocket) {
   // Client sends message to the server
@@ -15,4 +18,27 @@ wss.on("connection", function connection(client: WebSocket) {
       }
     });
   });
+});
+
+// Express server for static client
+
+const app = express();
+const staticPort = 8080;
+
+app.use("/static", express.static(path.resolve(__dirname, "..", "client")));
+
+app.get("/", (req: Request, res: Response) => {
+  res.sendFile(path.resolve(__dirname, "..", "client", "index.html"));
+});
+
+app.post("/ws", (req: Request, res: Response) => {
+  //code to perform particular action.
+  //To access POST variable use req.body()methods.
+  res.json({
+    ws: `${process.env.WEBSOCKET_PROTOCOL}://${process.env.URL}:${process.env.WEBSOCKET_PORT}`,
+  });
+});
+
+app.listen(staticPort, () => {
+  console.log(`Static website istening at http://localhost:${staticPort}`);
 });
